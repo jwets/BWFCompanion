a658d1e38ffa0557631d74bb75e41d161f5e16a8
Julien Wets <julien.wets@gmail.com>
2020-11-08 10:42:41 +0100

    initial commit

---
+++ frontend/static/js/index.js
@@ -0,0 +1,29 @@
+const router = async() => {
+    const routes = [
+        {path: "/", view: () => console.log("Viewing Dashboard")},
+        {path: "/posts", view: () => console.log("Viewing Posts")},
+        {path: "/settings", view: () => console.log("Viewing Settings")},
+    ]
+
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        }
+    })
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch)
+
+    if (!match) {
+        match = {
+            route: routes[0],
+            isMatch: true
+        }
+    }
+
+    console.log(match.route.view())
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    router()
+})
